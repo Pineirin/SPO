17e1fc16c767f26130098c9333c722813e3491e9
Pablo García Marcos <pineirin@gmail.com>
2019-12-08 16:27:35 +0100

    Create NamesSalary.java

---
+++ NamesSalary.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class NamesSalary {
+
+	public static class NamesMapper extends Mapper<Object, Text, Text, IntWritable> {
+		
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+	
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] line = value.toString().split(",");
+
+			// The output "state"
+			String et = line[2];
+			word.set(et);
+
+			// The output "product"
+			String year = line[0];
+      String year2 = line[6];
+			if(year.equals(year2)){
+				// Record the output in the Context object
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class NamesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+		
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {		
+		// Create the job specification object
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "Ethnic variaty");
+		job.setJarByClass(NamesSalary.class);
+
+		// Setup input and output paths
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// Set the Mapper and Reducer classes
+		job.setMapperClass(NamesMapper.class);
+		job.setReducerClass(NamesReducer.class);
+
+		// Specify the type of output keys and values
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// Wait for the job to finish before terminating
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
