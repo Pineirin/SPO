d7fbd2712ab3a0874beceac8d741ad1a6c37e5d6
Pablo García Marcos <pineirin@gmail.com>
2019-12-09 19:41:10 +0100

    Update LiveAnimals.java

--- LiveAnimals.java
+++ LiveAnimals.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,34 +14,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class LiveAnimals {
 
-	public static class Mapper1 extends Mapper<Object, Text, Text, Text> {
+	public static class Mapper1 extends Mapper<Object, Text, Text, IntWritable> {
 		
 		private Text aclass = new Text();
-		private Text one = new Text();
+		private final static IntWritable one = new IntWritable(1);
 	
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			String[] line = value.toString().split(",");
 
 			// The output "state"
 			String _aclass = line[5];
-			aclass.set(_class);
+			aclass.set(_aclass);
 
 			// The output "product"
 			String family = line[3];
 			if(family.equals("Live animals")){
 				// Record the output in the Context object
-				context.write(class, one);
+				context.write(aclass, one);
 			}
 		}
 	}
 
-	public static class Reducer1 extends Reducer<Text, Text, Text, Text> {
-		private Text result = new Text();
+	public static class Reducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
 		
 		@Override
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
-			for (Text val : values) {
+			for (IntWritable val : values) {
 				sum += val.get();
 			}
 			result.set(sum);
@@ -64,7 +65,7 @@ public class LiveAnimals {
 
 		// Specify the type of output keys and values
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 
 		// Wait for the job to finish before terminating
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
