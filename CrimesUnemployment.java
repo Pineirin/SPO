21349c26f6d81c9bf0610ad885eb62573707f74d
Pablo García Marcos <pineirin@gmail.com>
2019-12-10 11:35:55 +0100

    Update CrimesUnemployment.java

--- CrimesUnemployment.java
+++ CrimesUnemployment.java
@@ -26,13 +26,30 @@ public class ViolentCrimesUnemployment {
 			String currentYear = line[0];
 			if (currentYear.equals("2011") || currentYear.equals("2012") || currentYear.equals("2013")) {
 				year.set(currentYear);
-        crimes.set(line[1]);
+        crimes.set("Violent crimes rate: " + line[1] + "%");
 			  context.write(year, crimes);
 			  }
 		}
 	}
 	
 	public static class Mapper2 extends Mapper<Object, Text, Text, Text> {
+		
+		private Text crimes = new Text();
+		private Text year = new Text();
+	
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] line = value.toString().split(",");
+
+			String currentYear = line[0];
+			if (currentYear.equals("2011") || currentYear.equals("2012") || currentYear.equals("2013")) {
+				year.set("currentYear);
+        crimes.set("Non violent crimes: " + line[1] + "%");
+			  context.write(year, crimes);
+			  }
+		}
+	}
+	
+	public static class Mapper3 extends Mapper<Object, Text, Text, Text> {
 		
 		private Text unemployment = new Text();
 		private Text year = new Text();
@@ -53,30 +70,43 @@ public class ViolentCrimesUnemployment {
 	public static class Reducer1 extends Reducer<Text, Text, Text, Text> {		
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			String[] val = new String[2];
+			String[] val = new String[3];
 			int i = 0;
 			for (Text n : values) {
 				val[i] = n.toString();
 				i++;
 			}
-						if (val[1].contains("%")){
-				context.write(key, new Text("Violent crimes rate: " + val[0] + "% Unemployment rate" + val[1]));
-			} else {
-context.write(key, new Text("Violent crimes rate: " + val[1] + "% Unemployment rate" + val[0]));
-}
+			String VC = "";
+			String NVC = "";
+			String UR = "";
+			for (int i = 0; i<val.length; i++){
+				if (val[i].contains("Violent crimes")){
+					if (val[i].contains("Non")){
+						NVC = val[i];
+					}
+					else {
+						VC = val[i]
+					}
+				}
+				else {
+					UR = val[i];
+				}
+			}
+			context.write(key, new Text("Unemployment rate: " + UR + " " + NVC + " " + VC));
 		}
 	}
 
 	public static void main(String[] args) throws Exception {		
 		// Create the job specification object
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "Violent Crimes Unemployment");
+		Job job = Job.getInstance(conf, "Crimes Unemployment");
 		job.setJarByClass(ViolentCrimesUnemployment.class);
 
 		// Setup input and output paths
 		MultipleInputs.addInputPath(job,new Path(args[0]), TextInputFormat.class, Mapper1.class);
 		MultipleInputs.addInputPath(job,new Path(args[1]), TextInputFormat.class, Mapper2.class);
-		FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		MultipleInputs.addInputPath(job,new Path(args[2]), TextInputFormat.class, Mapper3.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[3]));
 		
 		job.setReducerClass(Reducer1.class);
 
